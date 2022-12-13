4b1f56b5a0e18f1a3f5cef14d0c18c961bd99b19
alexiabayol <alexia.bayol@hotmail.fr>
2022-12-13 11:35:14 +0100

    Premier commit, implémentation des classes

--- src/main/java/champollion/ServicePrevu.java
+++ src/main/java/champollion/ServicePrevu.java
@@ -1,6 +1,54 @@
 package champollion;
 
 public class ServicePrevu {
-	// TODO : implémenter cette classe
+    
+        private UE ue;
+	private final Enseignant enseignants;
+
+	private int CM;
+	private int TD;
+	private int TP;
+
+	public ServicePrevu(Enseignant enseignants, UE ue, int CM, int TD, int TP){
+		this.enseignants=enseignants;
+		this.ue=ue;
+		this.CM=CM;
+		this.TD=TD;
+		this.TP=TP;
+	}
+
+    public UE getUE() {
+        return ue;
+    }
+
+    public void setUE(UE ue) {
+        this.ue = ue;
+    }
+
+    public int getCM() {
+        return CM;
+    }
+
+    public void setCM(int CM) {
+        this.CM = CM;
+    }
+
+    public int getTD() {
+        return TD;
+    }
+
+    public void setTD(int TD) {
+        this.TD = TD;
+    }
+
+    public int getTP() {
+        return TP;
+    }
+
+    public void setTP(int TP) {
+        this.TP = TP;
+    }
 
 }
+
+	
