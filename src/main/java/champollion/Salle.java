411c64387f04441ed3b5101c15a5b3855acaa5bb
alexiabayol <alexia.bayol@hotmail.fr>
2022-12-13 11:35:27 +0100

    Premier commit, implémentation des classes

---
+++ src/main/java/champollion/Salle.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package champollion;
+
+/**
+ *
+ * @author Alexia
+ */
+public class Salle {
+    private String intitule;
+    private int capacite;
+
+    public Salle(String intitule, int capacite) {
+        this.intitule = intitule;
+        this.capacite = capacite;
+    }
+
+    public String getIntitule() {
+        return intitule;
+    }
+
+    public void setIntitule(String intitule) {
+        this.intitule = intitule;
+    }
+
+    public int getCapacite() {
+        return capacite;
+    }
+
+    public void setCapacite(int capacite) {
+        this.capacite = capacite;
+    }
+    
+    
+}
