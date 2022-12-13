160c0f0834eb63b399ff81ba53990f874a604bcb
alexiabayol <alexia.bayol@etud.univ-jfc.fr>
2022-12-13 12:18:54 +0100

    Ajouts de tests

--- src/test/java/champollion/ChampollionJUnitTest.java
+++ src/test/java/champollion/ChampollionJUnitTest.java
@@ -1,5 +1,6 @@
 package champollion;
 
+import java.util.Date;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -37,4 +38,36 @@ public class ChampollionJUnitTest {
 		
 	}
 	
+        @Test
+	public void testHeuresPrevues(){
+		//On met 8h de CM, 10h de TD et 12h de TP pour UML
+		untel.ajouteEnseignement(uml, 8, 10, 12);
+
+		//On met 5h de CM, 10h de TD, 15h de TP pour JAVA
+		untel.ajouteEnseignement(java, 5, 10, 15);
+                
+                //Le total est de 60h
+		assertEquals(60, untel.heuresPrevues(),"L'enseignant doit avoir 60 h de prévues");
+	}
+        
+        @Test
+	public void testSousService(){
+		//On met 8h de CM, 10h de TD, 12h de TP pour UML (le tout infénieur à 192h)
+		untel.ajouteEnseignement(uml, 8, 10, 12);
+		assertTrue(untel.enSousService(),"L'enseignant doit être en sous-service");
+
+		//On met 50h de CM, 40h de TD et 105h de TP pour UML (le tout supérieur à 192h)
+		untel.ajouteEnseignement(uml, 50, 40, 105);
+		assertFalse(untel.enSousService(),"L'enseignant doit être en sous-service");
+	}
+        
+        @Test
+	public void testAjoutInterventionUEInvalide(){
+		untel.ajouteEnseignement(uml, 10, 10, 10);
+		try{
+			untel.ajouteIntervention(new Intervention(new Date(), 1, 14, TypeIntervention.TD, java, new Salle("B019", 30)));
+			fail("Cet appel doit lever une exception");
+		} catch (IllegalArgumentException e){
+		}
+        }
 }
